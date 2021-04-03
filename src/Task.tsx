2b900186f7c86d0af95918ac83a00cab13a80046
manudevelopia <manu@developia.info>
2021-04-03 12:52:38 +0200

    use Task object to parse api response

---
+++ src/Task.tsx
@@ -0,0 +1,8 @@
+interface Task {
+  tid?: string,
+  title: string,
+  description?: string,
+  done?: boolean
+}
+
+export default Task;
--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -1,14 +1,20 @@
 import React, {FC} from "react";
+import Task from "./Task";
 
 interface TaskListProps {
-  tasks: string[];
+  tasks: Task[];
 }
 
 export const TaskList: FC<TaskListProps> = ({tasks}) =>
   <div>
     <ul>
       {tasks.map((task, index) =>
-        <li key={index}>{task}</li>
+        <li key={index}>
+          <div>{task.tid}</div>
+          <div>{task.title}</div>
+          <div>{task.description}</div>
+          <div>{task.done}</div>
+        </li>
       )}
     </ul>
   </div>;
